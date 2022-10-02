937128f1b497cc4cd44eb51c9fc1589df9b05990
kavyakallagunta <94386680+kavyakallagunta@users.noreply.github.com>
2022-10-02 06:31:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import B from './B';
+
+// import LifecycleClass from "./LifecycleClass";
+import LifecycleFunc from "./LifecycleFun";
+
+// import StuentList from "./StudentList";
+// import StudentListClass from "./StudentListClass";
+// import StudentListfunction from "./StudentListfunction";
+
+// const App = () => {
+//     return (
+//         <div>
+//             <h3>this is App componenet</h3>
+//             <B value={'SVECW'}/>
+//         </div>
+//     );
+// };
+
+// export default App;
+
+function App() {
+    return (
+      <div className="App">
+        <div>
+          
+         {/* <StuentList /> */}
+         {/* <StudentListClass/> */}
+         {/* <StudentListfunction/> */}
+         {/* <LifecycleClass/> */}
+         <LifecycleFunc/>
+           
+        </div>
+      </div>
+    );
+  }
+    
+  export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import {createContext} from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// export const college=React.createContext();
+// const root =ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//     // <React.StrictMode>
+//         // <college.Provider value={'SVECW'}>
+//         // </college.Provider>
+//         <App />
+//     // </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root =ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    
+        <App />
+   
+);
